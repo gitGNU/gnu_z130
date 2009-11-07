0168078065e992a9e64f5bbd24c43f199452bc81
Mario Castelan Castro <mariocastelancastro@gmail.com>
2009-11-06 20:39:13 -0600

    version.h para indicar la versión del programa

---
+++ version.h
@@ -0,0 +1,20 @@
+/* version.h - Version for Zirrux 130
+ * Copyright (C) 2009 Mario Castelan Castro
+ *
+ * This file is part of Zirrux 130
+ *
+ * Zirrux 130 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Zirrux 130 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this Zirrux 130.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define Z130_VERSION "desarollo"
